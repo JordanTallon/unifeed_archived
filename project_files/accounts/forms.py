9463c4d9f86812d40f91efaed9ed211d729ec7dd
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-01-03 22:07:27 +0000

    Add login form

--- project_files/accounts/forms.py
+++ project_files/accounts/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import User
 
 
@@ -8,3 +8,10 @@ class UserRegistrationForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1',
                   'password2', 'track_analytics']
+
+
+class UserLoginForm(AuthenticationForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
