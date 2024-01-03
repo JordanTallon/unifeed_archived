3311120871dd8db687fb11b740c232629151db5e
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-01-03 21:27:12 +0000

    Add POST view to handle registration form POST request

--- project_files/accounts/views.py
+++ project_files/accounts/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm
 # Create your views here.
 
 
 def registration(request):
     form = UserRegistrationForm()
+
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            # Redirect to the login page after valid registration
+            return redirect('/')
+
     context = {'form': form}
     return render(request, "accounts/registration.html", context)
