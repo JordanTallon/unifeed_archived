73b806f31f80a7bb7d991ebb6eeae0569af7db03
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-01-04 13:53:18 +0000

    Add login with invalid user unit test

--- project_files/accounts/test/test_login.py
+++ project_files/accounts/test/test_login.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 
 class LoginTest(TestCase):
 
+    # Initial account set up that can be used for each test
     def setUp(self) -> None:
         self.form_class = UserLoginForm
         self.username = "testaccount123"
@@ -19,15 +20,35 @@ class LoginTest(TestCase):
             password=self.password
         )
 
+    # Test if the view correctly returns the 'login' page for the 'login' route
     def test_login_page_correct_response(self):
         response = self.client.get(reverse('login'))
         self.assertTemplateUsed(response, 'accounts/login.html')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
+    # Ensure that the login form has a username and password field
     def test_user_login_form_has_expected_fields(self):
         self.assertTrue(issubclass(self.form_class, UserLoginForm))
         self.assertTrue('username' in self.form_class.Meta.fields)
         self.assertTrue('password' in self.form_class.Meta.fields)
+
+    def test_login_with_invalid_user(self):
+        user_data = {
+            'username': '--',  # Invalid username
+            'password': self.password
+        }
+
+        response = self.client.post(reverse('login'), user_data)
+
+        # The user remains on the login page
+        self.assertEqual(response.status_code, 200)
+
+        # An error message is displayed to the user
+        self.assertContains(
+            response, 'Please enter a correct username and password.')
+
+        # The user is not logged in
+        self.assertFalse('_auth_user_id' in self.client.session)
 
     def test_login_with_valid_user(self):
         user_data = {
@@ -37,4 +58,8 @@ class LoginTest(TestCase):
 
         response = self.client.post(reverse('login'), user_data)
 
+        # The user is logged in
+        self.assertTrue('_auth_user_id' in self.client.session)
+
+        # The login page redirected to the home page after successful login
         self.assertRedirects(response, reverse('home'))
