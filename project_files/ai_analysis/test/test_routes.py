dd4897dbaff26d6c2232560773eacd8cab732ca7
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-01-12 21:17:18 +0000

    Add unit tests for 'add-political-bias' - check the  post route existence, and posting a new object to the route

--- project_files/ai_analysis/test/test_routes.py
+++ project_files/ai_analysis/test/test_routes.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from .factories import PoliticalBiasAnalysisFactory
+from ..models import PoliticalBiasAnalysis
 import json
 
 
@@ -26,3 +27,29 @@ class test_political_bias_analysis_api_routes(TestCase):
 
         # Make sure all 50 models are serialized in the data (batch size in setUp).
         self.assertEqual(len(response_data), 50)
+
+    def test_detect_political_bias_api_post_exists(self):
+        # Create a POST request.
+        response = self.client.post(reverse('add-political-bias'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_detect_political_bias_api_post_new_data(self):
+        # Get the count of objects before new addition
+        length = len(PoliticalBiasAnalysis.objects.all())
+
+        # Data for the new model
+        data = {
+            'article_url': 'www.example.com',
+            'article_text_md5': '1234123',
+            'political_bias': 100
+        }
+
+        # Post the data to the url
+        response = self.client.post(reverse('add-political-bias'), data=data)
+
+        # 201 = created
+        self.assertEqual(response.status_code, 201)
+
+        # Check if the objects contain the newly posted object (length + 1)
+        new_length = len(PoliticalBiasAnalysis.objects.all())
+        self.assertEqual(new_length, length + 1)
