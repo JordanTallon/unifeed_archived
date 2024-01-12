8eaa82cbff1b373398cf668e50a8093caa9c0d1f
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-01-12 20:59:11 +0000

    Add unit tests for /get-political-bias/ get route existence, and JSON data return

--- project_files/ai_analysis/test/test_routes.py
+++ project_files/ai_analysis/test/test_routes.py
@@ -1,13 +1,30 @@
-from django.test import TestCase
-
-# Create your tests here.
+from django.test import TestCase, Client
+from django.urls import reverse
+from .factories import PoliticalBiasAnalysisFactory
+import json
 
 
 class test_political_bias_analysis_api_routes(TestCase):
-    def test_detect_political_bias_api_get(self):
-        pass
+    def setUp(self):
+        self.client = Client()
+        # Add 50 instances of the PoliticalBiasAnalysis model with dummy data (see factories.py)
+        PoliticalBiasAnalysisFactory.create_batch(50)
 
+    def test_detect_political_bias_api_get_exists(self):
+        # Create a GET request.
+        response = self.client.get(reverse('get-political-bias'))
+        self.assertEqual(response.status_code, 200)
 
-"""     response = self.client.get(reverse('login'))
-    self.assertTemplateUsed(response, 'accounts/login.html')
-    self.assertEqual(response.status_code, HTTPStatus.OK) """
+    def test_detect_political_bias_get_returns_serialized_data(self):
+        # Create a GET request.
+        response = self.client.get(reverse('get-political-bias'))
+
+        print(response.content)
+        print(response.status_code)
+        self.assertEqual(response.status_code, 200)
+
+        # Read the content of the response as json.
+        response_data = json.loads(response.content.decode('utf-8'))
+
+        # Make sure all 50 models are serialized in the data (batch size in setUp).
+        self.assertEqual(len(response_data), 50)
