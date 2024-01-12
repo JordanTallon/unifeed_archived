10f7951cbea18e5e266c93b9b149f44f312954b7
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-01-12 21:06:47 +0000

    Implement get-political-bias get view to return all PoliticalBiasAnalysis objects as JSON

--- project_files/ai_analysis/views.py
+++ project_files/ai_analysis/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import PoliticalBiasAnalysis
+from .serializer import PoliticalBiasAnalysisSerializer
 
 
 @api_view(['GET'])
 def getData(request):
-    return Response()
+    # Get all PoliticalBiasAnalysis objects
+    biases = PoliticalBiasAnalysis.objects.all()
+    # Apply the serializer to the entire array
+    serializer = PoliticalBiasAnalysisSerializer(biases, many=True)
+    # Return serialized data
+    return Response(serializer.data)
