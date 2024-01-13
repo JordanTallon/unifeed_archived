e596b681610f8c26c02ed57ea3932f2fc0ad06c7
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-01-13 04:31:23 +0000

    Add unit test for PoliticalBiasAnalysis model to test md5 hashing functionality

--- project_files/ai_analysis/test/test_models.py
+++ project_files/ai_analysis/test/test_models.py
@@ -11,7 +11,7 @@ class PoliticalBiasAnalysisModelTest(TestCase):
 
     def test_add_new_political_bias_analysis(self):
         bias = PoliticalBiasAnalysis(
-            article_url="example.com",
+            article_url="http://example.com",
             article_text_md5="0e7807a9fc2fc9c1acff9e5560e1de24",
             political_bias=50
         )
@@ -21,3 +21,15 @@ class PoliticalBiasAnalysisModelTest(TestCase):
         added_bias = PoliticalBiasAnalysis.objects.first()
 
         self.assertIsNotNone(added_bias)
+
+    def test_generate_article_text_md5(self):
+        bias = PoliticalBiasAnalysis(
+            article_url="http://example.com",
+            political_bias=50
+        )
+        # Generate an md5 hash for the model from the string "Hello World"
+        bias.article_text_to_md5_hash("Hello World")
+
+        # Check if the hash is equal to the hash value for "Hello World" (deterministic)
+        self.assertEqual(bias.article_text_md5,
+                         'b10a8db164e0754105b7a99be72e3fe5')
