e40ec754b7c37488489111239b2574f63bafafee
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-01-13 04:32:10 +0000

    Implement PoliticalBiasAnalysis model article_text_to_md5_hash function

--- project_files/ai_analysis/models.py
+++ project_files/ai_analysis/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import hashlib
 
 
 class PoliticalBiasAnalysis(models.Model):
@@ -7,6 +8,15 @@ class PoliticalBiasAnalysis(models.Model):
     # Stores the article text as a md5 hash
     article_text_md5 = models.CharField(max_length=32)
     political_bias = models.IntegerField()
+
+    # Function to assign the article_text_md5 hash based on the article text (using Python hashlib)
+    def article_text_to_md5_hash(self, article_text):
+        # Encode article text
+        encoded_text = article_text.encode()
+        # Convert to md5 hash
+        md5_hash_result = hashlib.md5(encoded_text)
+        # Digest the hash as hexadecimal and assign it to article_text_md5
+        self.article_text_md5 = md5_hash_result.hexdigest()
 
     class Meta:
         # Override default admin display "Political bias analysiss" which is incorrect / ugly
