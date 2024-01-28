f9e16b1200431c0ba7f9a7a6370d6b0b078c3cd6
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-01-28 23:11:38 +0000

    Add post view unit test for scraper.views

--- project_files/scraper/tests.py
+++ project_files/scraper/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.db import IntegrityError
+from django.core.exceptions import ValidationError
+from django.urls import reverse
 
-# Create your tests here.
+
+class ScraperViewsTest(TestCase):
+    def test_scaper_post_response(self):
+        response = self.client.post(
+            reverse('scrape'), {'url': 'https://example.com'})
+
+        content = response.json()['content']
+
+        self.assertIn(
+            "This domain is for use in illustrative examples in documents", content)
