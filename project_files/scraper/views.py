6fdfa47a5340726a77cbcae076431276c7506e66
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-01-28 23:39:06 +0000

    Switch scraper.views to use django-rest-framework (much simpler)

--- project_files/scraper/views.py
+++ project_files/scraper/views.py
@@ -1,10 +1,20 @@
 # scraper/views.py
 
 from django.shortcuts import render
-from django.http import JsonResponse
 from bs4 import BeautifulSoup
 import requests
 import json
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['POST'])
+def scrape(request):
+    data = json.loads(request.body)
+    url = data.get('url')
+    if url:
+        content = scrape_data(url)
+        return Response({'url': url, 'content': content})
 
 
 def scrape_data(url):
@@ -12,18 +22,3 @@ def scrape_data(url):
     soup = BeautifulSoup(response.text, 'html.parser')
     content = ' '.join([p.get_text() for p in soup.find_all('p')])
     return content
-
-
-def scrape(request):
-    if request.method == 'POST':
-        try:
-            data = json.loads(request.body)
-            url = data.get('url')
-            if url:
-                content = scrape_data(url)
-                return JsonResponse({'url': url, 'content': content})
-        except:
-            return JsonResponse({'error': 'Method not allowed'}, status=405)
-    else:
-        # No get route for scrape, instead display an error (might require get route later?)
-        return JsonResponse({'error': 'Method not allowed'}, status=405)
