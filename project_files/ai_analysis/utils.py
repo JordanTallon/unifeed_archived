dece0b32ec317afdeabe7da59d8047c1efa4d044
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-01-29 13:08:01 +0000

    Move analyzePoliticalBias to ai_analysis.utils

--- project_files/ai_analysis/utils.py
+++ project_files/ai_analysis/utils.py
@@ -1,4 +1,6 @@
 import hashlib
+import requests
+from django.conf import settings
 
 
 # Function to assign the given text to a hash (using Python hashlib)
@@ -9,3 +11,30 @@ def text_to_md5_hash(self, text):
     md5_hash_result = hashlib.md5(encoded_text)
     # Digest the hash as a hexadecimal and return it
     return md5_hash_result.hexdigest()
+
+
+def analyzePoliticalBias(self, article_text):
+    # TODO: I will find 'ideal' sentences from an article
+    sentences = article_text.split('.')
+
+    # Restrict to 5 sentences per article (5 is arbitrary for testing, i'm not sure what a good limit is yet)
+    if (len(sentences) >= 5):
+        sentences = sentences[:5]
+
+    def query(payload):
+        response = requests.post(settings.HUGGINGFACE_API_URL,
+                                 headers=settings.HUGGINGFACE_API_HEADERS, json=payload)
+        return response.json()
+
+    # Query for all sentences and gather the results in a 'results' array
+    results = []
+    for sentence in sentences:
+        output = query({"inputs": sentence})
+        results.append(output)
+
+    # Associate each result with the sentence text. For displaying to the user later on.
+    result_dict = {}
+    for i in range(5):
+        result_dict[sentences[i]] = results[i]
+
+    return result_dict
