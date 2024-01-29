da9a8c35ce7beb4ddb688cfc4a85384c0605dece
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-01-29 19:00:13 +0000

    Fix ai_analysis imports, biased_sentences default value

--- project_files/ai_analysis/models.py
+++ project_files/ai_analysis/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from jsonfield import JSONField
 from scraper.utils import scrape_data
-from utils import text_to_md5_hash, analyze_political_bias
+from ai_analysis.utils import text_to_md5_hash, analyze_political_bias
 
 
 class PoliticalBiasAnalysis(models.Model):
@@ -10,9 +10,7 @@ class PoliticalBiasAnalysis(models.Model):
     # Stores the article text as a md5 hash
     article_text_md5 = models.CharField(max_length=32)
 
-    article_text = models.TextField()
-
-    biased_sentences = models.JSONField()
+    biased_sentences = models.JSONField(default=dict)
 
     @classmethod
     def create(cls, article_url):
@@ -23,7 +21,7 @@ class PoliticalBiasAnalysis(models.Model):
 
         biased_sentences = analyze_political_bias(article_text)
 
-        return cls(article_text=article_text, article_text_md5=article_text_md5, biased_sentences=biased_sentences)
+        return cls(article_url=article_url, article_text_md5=article_text_md5, biased_sentences=biased_sentences)
 
     class Meta:
         # Override default admin display "Political bias analysiss" which is incorrect / ugly
