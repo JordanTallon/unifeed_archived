6a7d57a703a19cfc8d6a73aff4c7c071083b028d
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-01 12:14:52 +0000

    Add Scraper unit test for invalid URL handling

--- project_files/scraper/tests.py
+++ project_files/scraper/tests.py
@@ -5,9 +5,19 @@ from scraper.utils import scrape_data
 
 
 class ScraperViewsTest(TestCase):
-    def test_scaper_post_response(self):
+    def test_scaper_succesful_response(self):
 
         content = scrape_data('https://example.com')
 
         self.assertIn(
             "This domain is for use in illustrative examples in documents", content)
+
+    def test_scaper_bad_url_response(self):
+
+        with self.assertRaises(ValueError):
+            scrape_data('1')
+
+    def test_scaper_non_existent_url_response(self):
+
+        with self.assertRaises(ValueError):
+            scrape_data('https://www.ca326unifeedprojecttesting.com/')
