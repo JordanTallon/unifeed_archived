fd5a846a4c2216288489c3813ad2be20e55f0809
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-03 22:07:42 +0000

    Add 'only_public' paths to RequireLoginMiddleware (paths only non logged in users can view)

--- project_files/unifeed/middleware.py
+++ project_files/unifeed/middleware.py
@@ -17,12 +17,24 @@ def RequireLoginMiddleware(get_response):
             reverse('register')
         ]
 
+        only_public_paths = [
+            reverse('login'),
+            reverse('register')
+        ]
+
         # If the user isn't logged in
         if not request.user.is_authenticated:
             # And the path they are trying to view isn't in the 'public_paths' list
             if request.path not in public_paths:
                 # Redirect them to the login page
                 return redirect(settings.LOGIN_URL)
+        # If instead, the user is authenticated
+        else:
+            # And they are trying to view a 'only_public' path
+            if request.path in only_public_paths:
+                # Redirect them to the home page
+                # This prevents already logged in users from 'logging in' or making another account
+                return redirect('home')
 
         return response
 
