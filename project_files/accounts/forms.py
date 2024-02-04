deff96e9048a318969af4fa038dfb37e24ef41c9
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-04 19:38:42 +0000

    Add password changing to account settings

--- project_files/accounts/forms.py
+++ project_files/accounts/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
 from django.contrib.auth import get_user_model
+from django import forms
 
 from .models import User
 
@@ -20,6 +21,40 @@ class UserLoginForm(AuthenticationForm):
 
 
 class AccountSettingsForm(UserChangeForm):
+
+    password1 = forms.CharField(
+        label='New password',
+        widget=forms.PasswordInput
+    )
+
+    password2 = forms.CharField(
+        label='New password confirmation',
+        widget=forms.PasswordInput
+    )
+
     class Meta:
         model = get_user_model()
         fields = ['username', 'email']
+
+    # https://docs.djangoproject.com/en/5.0/ref/forms/validation/ (for reminder on clean, TLDR: it runs automatically during form validation)
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+        # If both passwords were provided
+        if password1 and password2:
+            # Return the cleaned data if they match, otherwise continue to error
+            if password1 == password2:
+                return cleaned_data
+
+        # Either the passwords didn't match or one was missing
+        self.add_error('password2', "Passwords don't match.")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password1 = self.cleaned_data.get('password1')
+        if password1:
+            user.set_password(password1)
+        if commit:
+            user.save()
+        return user
