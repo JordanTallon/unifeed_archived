1d9fd4f95bbcdc13cb40058a7059cac3bab45f2e
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-05 21:40:28 +0000

    Move FeedItem from rss_app.models.FeedItem to replace feeds.models.FeedItem (not sure why there were 2 versions of the same model)

--- project_files/feeds/models.py
+++ project_files/feeds/models.py
@@ -1,5 +1,6 @@
-# folder_system/models.py
 from django.db import models
+from django.urls import reverse
+
 
 class Folder(models.Model):
     name = models.CharField(max_length=400)
@@ -7,12 +8,19 @@ class Folder(models.Model):
     def __str__(self):
         return self.name
 
+
 class FeedItem(models.Model):
     title = models.CharField(max_length=400)
     description = models.TextField()
     link = models.URLField()
-    folder = models.ForeignKey(Folder, on_delete=models.CASCADE, null=True, blank=True)
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE,
+                               null=True, blank=True, related_name='rss_feed_items')
+
+    def get_absolute_url(self):
+        if self.folder:
+            return reverse('feeds_in_folder', args=[str(self.folder.id)])
+        else:
+            return reverse('index')
 
     def __str__(self):
         return self.title
-
--- project_files/rss_app/models.py
+++ project_files/rss_app/models.py
@@ -1,19 +1,4 @@
 # rss_app/models.py
 from django.db import models
-from folder_system.models import Folder
+from feeds.models import Folder
 from django.urls import reverse
-
-class FeedItem(models.Model):
-    title = models.CharField(max_length=400)
-    description = models.TextField()
-    link = models.URLField()
-    folder = models.ForeignKey(Folder, on_delete=models.CASCADE, null=True, blank=True, related_name='rss_feed_items')
-
-    def get_absolute_url(self):
-        if self.folder:
-            return reverse('feeds_in_folder', args=[str(self.folder.id)])
-        else:
-            return reverse('index')  
-
-    def __str__(self):
-        return self.title
