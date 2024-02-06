f03c05c94ef4b102b60c36fbec787a1d3ace956c
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-06 19:46:06 +0000

    Remove forms and temporarily disable url and views for feeds app

--- project_files/feeds/urls.py
+++ project_files/feeds/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('folders/', views.folder_list, name='folder_list'),
-    path('folders/create/', views.create_folder, name='create_folder'),
+    path('folders/', views.folder_list, name='folder_list')
 ]
--- project_files/feeds/views.py
+++ project_files/feeds/views.py
@@ -1,11 +1,17 @@
 # folder_system/view.py
 from django.shortcuts import render, redirect
 
-from .models import Folder
-from .forms import FolderForm
+from .models import *
 
 
 def folder_list(request):
+    return render(request, 'folder_system/folder_list.html')
+
+
+""" from .forms import FolderForm """
+
+
+""" def folder_list(request):
     folders = Folder.objects.all()
     return render(request, 'folder_system/folder_list.html', {'folders': folders})
 
@@ -20,3 +26,4 @@ def create_folder(request):
         form = FolderForm()
 
     return render(request, 'folder_system/create_folder.html', {'form': form})
+ """
