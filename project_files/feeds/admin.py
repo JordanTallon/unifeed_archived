5385de1fc26b09e11763803c4be4b0d6f538d8de
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-06 19:45:09 +0000

    Register feeds.models with feeds.admin

--- project_files/feeds/admin.py
+++ project_files/feeds/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Folder, FeedItem
+from .models import *
 
 # Register your models here.
-admin.site.register(Folder)
-admin.site.register(FeedItem)
+admin.site.register(FeedFolder)
+admin.site.register(Feed)
+admin.site.register(UserFeed)
+admin.site.register(Article)
