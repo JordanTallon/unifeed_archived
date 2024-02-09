ff5836e86d3f4e8c1d9644a004692b1b6172c3db
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-09 16:50:20 +0000

    Change Article publish_date to publish_datetime

--- project_files/articles/models.py
+++ project_files/articles/models.py
@@ -12,7 +12,7 @@ class Article(models.Model):
     description = models.TextField(blank=True, null=True)
     image_url = models.URLField(blank=True, null=True)
     author = models.CharField(max_length=255, blank=True, default='')
-    publish_date = models.DateField(blank=True, null=True)
+    publish_datetime = models.DateTimeField(blank=True, null=True)
     # Publisher would be nice to display. can be read from the title of the 'owning' rss feed?
     publisher = models.CharField(max_length=255, blank=True, default='')
 
