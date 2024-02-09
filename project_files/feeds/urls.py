c68022b7b89216e3adb45b4ad85372ab6ab852b2
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-09 12:24:15 +0000

    Update api urls, and new url for view_folder

--- project_files/feeds/urls.py
+++ project_files/feeds/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('import/feed', views.import_new_feed, name='import_new_feed'),
-    path('import/user_feed', views.import_user_feed, name='import_new_feed')
+    path('api/import/feed', views.import_new_feed, name='import_new_feed'),
+    path('api/import/user_feed', views.import_user_feed, name='import_new_feed'),
+    path('<int:user_id>/<int:folder_id>',
+         views.view_folder, name='view_folder'),
 ]
