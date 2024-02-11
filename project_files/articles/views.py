4b50fb82e65c892d10859097cb12b044d46bb4cd
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-11 19:38:29 +0000

    Add 'related articles' to article_details view

--- project_files/articles/views.py
+++ project_files/articles/views.py
@@ -8,11 +8,26 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
 from .models import Article
+from feeds.models import UserFeed
 from .utils import *
+import random
 
 
 @login_required
 def article_details(request, article_id):
     article = get_object_or_404(Article, pk=article_id)
 
-    return render(request, 'articles/article_details.html', {'article': article})
+    # Get "related" articles to the current article being viewed
+    # Just a random selection for now
+    user_feeds = UserFeed.objects.filter(
+        user=request.user).values_list('feed', flat=True)
+    related_articles_queryset = Article.objects.filter(
+        feeds__in=user_feeds
+    ).exclude(
+        image_url=""
+    ).distinct()
+    # Up to 10 articles
+    related_articles = random.sample(
+        list(related_articles_queryset), min(len(related_articles_queryset), 10))
+
+    return render(request, 'articles/article_details.html', {'article': article, 'related_articles': related_articles, })
