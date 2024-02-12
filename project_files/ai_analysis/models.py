68b1cdeb162d7d4e8ef1acef268c0b9287927e3f
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-12 15:16:30 +0000

    Refactor ai_analysis.models for asynchronous requests (see comments)

--- project_files/ai_analysis/models.py
+++ project_files/ai_analysis/models.py
@@ -2,14 +2,20 @@ from django.db import models
 from jsonfield import JSONField
 
 
-class PoliticalBiasAnalysis(models.Model):
-    article_url = models.URLField(max_length=200)
+class ArticleAnalysisResults(models.Model):
+    url = models.URLField(max_length=500)
 
     # Stores the article text as a md5 hash
     article_text_md5 = models.CharField(max_length=32)
 
-    biased_sentences = models.JSONField(default=dict)
+    # Will store each analyzed sentence and their bias values
+    sentence_results = models.JSONField(default=dict)
 
-    class Meta:
-        # Override default admin display "Political bias analysiss" which is incorrect / ugly
-        verbose_name_plural = "Political Bias Analysis Records"
+
+# This object is returned instantly to the user when they make a request to track the asynchronous bias analysis.
+# For example, the user submits a request, An instance of this model is created and returned to them,
+# The client reads the status as "processing" so their it begins to periodically make fetch requests until the status
+# is either "complete" (where they will then be presented with a ArticleAnalysisResults object) or "failed" where an error message will appear.
+class BiasAnalysis(models.Model):
+    url = models.URLField(max_length=500)
+    status = models.CharField(max_length=255)
