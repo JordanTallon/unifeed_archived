dfa08f7b65d7b7afa402e2813c0df751ec6f4196
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-12 19:20:00 +0000

    Remove debug coments from ai_analysis.tasks.py

--- project_files/ai_analysis/tasks.py
+++ project_files/ai_analysis/tasks.py
@@ -9,7 +9,6 @@ from .models import BiasAnalysis
 # with an asynchronous version that calls it but does not interfere with its functionality or unit tests
 @shared_task
 def scrape(url, analysis_id):
-    print("starting scrape")
     try:
         article_text = scrape_data(url)
 
@@ -19,13 +18,11 @@ def scrape(url, analysis_id):
         return
 
     sentences = extract_ideal_sentences(article_text)
-    print("scrape results", sentences)
     analyse_sentences.delay(sentences, analysis_id)
 
 
 @shared_task
 def analyse_sentences(sentences, analysis_id):
-    print("starting analysis")
     results = analyse_sentences_for_bias(sentences)
 
     print("analysis results", results)
