1f68b819ae141e03739f7662f2ec17cb2ff34fcc
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-12 14:12:39 +0000

    Add celery starter to __init__.py and redis configuration to settings.py

---
+++ project_files/unifeed/__init__.py
@@ -0,0 +1,6 @@
+# From: https://docs.celeryq.dev/en/stable/django/first-steps-with-django.html
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+from .celery import app as celery_app
+
+__all__ = ('celery_app',)
