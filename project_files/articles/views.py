81d63cf29a55777d6e35f3ce68ad9a870cdc267c
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-16 21:44:51 +0000

    Add save article post request (toggle save and unsaved)

--- project_files/articles/views.py
+++ project_files/articles/views.py
@@ -1,21 +1,30 @@
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from django.core.validators import URLValidator
-from django.core.exceptions import ValidationError
-from django.contrib.auth import get_user_model
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
-from .models import Article
-from feeds.models import UserFeed
+from .models import Article, SavedArticle
 from .utils import *
+from .forms import SaveArticleForm
 import random
 
 
 @login_required
 def article_details(request, article_id):
     article = get_object_or_404(Article, pk=article_id)
+
+    # This handles the user clicking on the "save article" button
+    if request.method == "POST":
+        form = SaveArticleForm(request.POST)
+        if form.is_valid():
+            # If the user already saved this article
+            already_saved = SavedArticle.objects.filter(
+                article=article, user=request.user)
+            if already_saved:
+                # 'Unsave' it (delete it)
+                already_saved.delete()
+            else:
+                # Save it
+                SavedArticle.objects.create(user=request.user, article=article)
+    else:
+        form = SaveArticleForm()
 
     # Get "related" articles to the current article being viewed
     # Just a random selection for now
@@ -25,4 +34,4 @@ def article_details(request, article_id):
     related_articles = random.sample(
         list(related_articles_queryset), min(len(related_articles_queryset), 5))
 
-    return render(request, 'articles/article_details.html', {'article': article, 'related_articles': related_articles, })
+    return render(request, 'articles/article_details.html', {'article': article, 'related_articles': related_articles, 'form': form})
