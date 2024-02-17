19f0e9b76d7b313129466ccc500ca30c90193764
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-17 15:27:40 +0000

    Add dynamic/live refresh feed button to load new articles

--- project_files/unifeed/settings.py
+++ project_files/unifeed/settings.py
@@ -75,6 +75,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     #   'unifeed.middleware.RequireLoginMiddleware',
 ]
