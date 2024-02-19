92d32cb9dab9d20b93a77592e25fa3eda5d3a80e
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-19 14:45:26 +0000

    Implement deleting all user reading history when the user disables history tracking

--- project_files/accounts/models.py
+++ project_files/accounts/models.py
@@ -1,10 +1,24 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from articles.models import RecentlyRead
 
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     track_history = models.BooleanField(default=False)
 
-    def toggle_track_history(self):
-        self.track_history = not self.track_history
+    def __init__(self, *args, **kwargs):
+        super(User, self).__init__(*args, **kwargs)
+        # store the original track history value when the account is made
+        self.__original_track_history = self.track_history
+
+    def save(self, *args, **kwargs):
+        # If track_history was True and now its being set to False
+        if self.__original_track_history and not self.track_history:
+            # Delete all reading history
+            RecentlyRead.objects.filter(user=self).delete()
+
+        super(User, self).save(*args, **kwargs)
+
+        # set the original track history to the new one
+        self.__original_track_history = self.track_history
