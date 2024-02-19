2fafc72f26b9fd5c30446518d479815ed5e8a55c
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-19 14:03:18 +0000

    Add read_article view and url

--- project_files/articles/urls.py
+++ project_files/articles/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('view/<int:article_id>/', views.article_details, name='article_details'),
-    path('reading_list/', views.saved_articles, name="reading_list")
+    path('reading_list/', views.saved_articles, name="reading_list"),
+    path('read_article/<int:article_id>/',
+         views.read_article, name="read_article")
 ]
