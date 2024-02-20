a6505cf30a835cfc7b03cf657d5fe644c73668be
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-20 16:06:08 +0000

    Add user to AIAnalysisFeedback to prevent a user from submitting multiple feedbacks for the same sentence

--- project_files/ai_analysis/models.py
+++ project_files/ai_analysis/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-
+from django.conf import settings
 
 # This object is returned instantly to the user when they make a post request to analyze the bias of an article.
 # For example: the user submits a request, An instance of this model is created and returned to them with an initial processing status.
 # The client reads the status as "processing" so it begins to periodically make fetch requests until the status
 # is either "complete" where they will then be presented with the results or "failed" where an error message will appear.
+
+
 class ArticleAnalysisResults(models.Model):
     url = models.URLField(max_length=500)
 
@@ -28,13 +30,23 @@ class ArticleAnalysisResults(models.Model):
 # derived Political Bias on a per sentence basis. This allows for UniFeed to gather data that
 # can be used to re-train and improve the prediction model over time
 class AIAnalysisFeedback(models.Model):
-    # The setence that was analyzed
+
+    # The sentence that was analyzed
     sentence = models.CharField(max_length=500)
+
     # The bias predicted for the sentence
     bias = models.CharField(max_length=6, blank=True)
+
     # The confidence of the AI prediction
     confidence = models.FloatField(default=0.0)
-    # Agree Count (how many users agree with the prediction)
+
+    # The user leaving the feedback
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+
+    # 0 if the user disagrees, 1 if they agree with the result
     agree = models.IntegerField(default=0)
-    # Disagree Count (how many users disagree with the prediction)
-    disagree = models.IntegerField(default=0)
+
+    class Meta:
+        # Only 1 feedback per user for each sentence
+        unique_together = ('user', 'sentence')
