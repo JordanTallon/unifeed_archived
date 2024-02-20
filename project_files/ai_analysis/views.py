6d90a205a2726385108433f1a0545d3a09becac8
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-20 16:52:20 +0000

    Add provide_analysis_feedback view

--- project_files/ai_analysis/views.py
+++ project_files/ai_analysis/views.py
@@ -1,16 +1,22 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-from .models import ArticleAnalysisResults
+from .models import ArticleAnalysisResults, AIAnalysisFeedback
 from .tasks import async_scrape
 from .serializer import ArticleAnalysisResultsSerializer
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 
 @api_view(['GET'])
 def check_analysis_status(request, analysis_id):
+    """     
+    Polled by HTMX to get a live update of the status. Returns the 'waiting_results' HTML until the status
+    is no longer 'processing', where it will return the final results, indicating success or failure 
+    """
     bias_analysis = ArticleAnalysisResults.objects.get(id=analysis_id)
     bias_analysis_serialized = ArticleAnalysisResultsSerializer(
         bias_analysis).data
@@ -21,6 +27,44 @@ def check_analysis_status(request, analysis_id):
     else:
         # Return a template with the final analysis results (success or fail, to be indicated in html)
         return render(request, 'ai_analysis/final_analysis_results.html', {'bias_analysis': bias_analysis_serialized})
+
+
+@login_required
+def provide_analysis_feedback(request):
+    """     
+    Polled by HTMX in get to return the 'ai_feedback.html' which contains the options to provide feedback.
+    This html is updated with a message saying feedback confirmed if the user posts here. 
+    Create an AIAnalysisFeedback object if it's their first time providing feedback for a particular sentence.
+    Otherwise, update their feedback to the new one.
+    """
+    if request.method == 'POST':
+        sentence = request.POST.get('sentence')
+        agree = int(request.POST.get('agree'))
+        bias = request.POST.get('bias')
+        confidence = request.POST.get('confidence')
+
+        # check if the feedback for this sentence already exists from the user
+        # if not, create a new one
+        feedback, created = AIAnalysisFeedback.objects.get_or_create(
+            user=request.user,
+            sentence=sentence,
+            defaults={
+                'agree': agree,
+                'confidence': confidence,
+                'bias': bias
+            }
+        )
+
+        # Update the feedback if it already existed
+        if not created:
+            feedback.agree = agree
+            feedback.save()
+
+        # provide the feedback details to the user.
+        context = {'feedback': feedback}
+        return render(request, 'ai_analysis/analysis_feedback.html', context)
+
+    return HttpResponse(status=405)
 
 
 @api_view(['POST'])
