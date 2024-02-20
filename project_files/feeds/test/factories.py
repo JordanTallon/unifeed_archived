dd84972e7a00d497f24357b195caf86fd17b00f6
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-20 20:18:30 +0000

    Change feeds.factories to respect model limits (char lengths) and to pass a User

--- project_files/feeds/test/factories.py
+++ project_files/feeds/test/factories.py
@@ -11,7 +11,7 @@ class FeedFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Feed
 
-    name = factory.LazyFunction(lambda: fake.catch_phrase())
+    name = factory.LazyFunction(lambda: fake.catch_phrase()[:15])
     link = factory.LazyFunction(lambda: fake.url())
     url = factory.LazyFunction(lambda: fake.url())
     description = factory.LazyFunction(lambda: fake.paragraph())
@@ -20,20 +20,27 @@ class FeedFactory(factory.django.DjangoModelFactory):
     last_updated = factory.LazyFunction(
         lambda: timezone.make_aware(fake.date_time()))
 
+    # Add 2 fake articles to each fake feed
+    articles = factory.RelatedFactoryList(
+        'articles.test.factories.ArticleFactory', factory_related_name='feed', size=2)
+
 
 class FeedFolderFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = FeedFolder
 
-    name = factory.LazyFunction(lambda: fake.catch_phrase())
+    name = factory.LazyFunction(lambda: fake.catch_phrase()[:15])
 
 
 class UserFeedFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = Feed
+        model = UserFeed
 
-    name = factory.LazyFunction(lambda: fake.catch_phrase())
+    # must be defined when calling to create the factory
+    user = None
+    name = factory.LazyFunction(lambda: fake.catch_phrase()[:15])
     description = factory.LazyFunction(lambda: fake.paragraph())
 
-    folder = factory.SubFactory(FeedFolderFactory)
+    folder = factory.SubFactory(
+        FeedFolderFactory, user=factory.SelfAttribute('..user'))
     feed = factory.SubFactory(FeedFactory)
