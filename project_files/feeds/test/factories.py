02153ab491b4953b1d47e3cab66b83b8d4cc9762
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2024-02-20 20:00:26 +0000

    Add FeedFolderFactory and UserFeedFactory

--- project_files/feeds/test/factories.py
+++ project_files/feeds/test/factories.py
@@ -2,7 +2,7 @@ import factory
 import factory.fuzzy
 from django.utils import timezone
 from faker import Faker
-from ..models import Feed
+from ..models import Feed, UserFeed, FeedFolder
 
 fake = Faker()
 
@@ -19,3 +19,21 @@ class FeedFactory(factory.django.DjangoModelFactory):
     publisher = factory.LazyFunction(lambda: fake.name())
     last_updated = factory.LazyFunction(
         lambda: timezone.make_aware(fake.date_time()))
+
+
+class FeedFolderFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = FeedFolder
+
+    name = factory.LazyFunction(lambda: fake.catch_phrase())
+
+
+class UserFeedFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Feed
+
+    name = factory.LazyFunction(lambda: fake.catch_phrase())
+    description = factory.LazyFunction(lambda: fake.paragraph())
+
+    folder = factory.SubFactory(FeedFolderFactory)
+    feed = factory.SubFactory(FeedFactory)
